ed5eb40af53e906f38405d66b52d026aa5fd9958
Sivan Jadhav <sivanjaadhav+git@gmail.com>
2024-02-22 11:25:18 +0530

    Restored Week 0 and Week 1 Files

---
+++ Week 1 - C/agree.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char c = get_char("Do you agree? ");
+
+    if (c == 'y' || 'Y')
+    {
+        printf("Agreed!\n");
+    }
+    else if (c == 'n' || 'N')
+    {
+        printf("Not Agreed!\n");
+    }
+}
---
+++ Week 1 - C/meow2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void meow(void);
+int main(void)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        meow();
+    }
+}
+
+void meow(void)
+{
+    printf("Meow!\n");
+}
